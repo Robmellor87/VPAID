92310bc013a70532c56e1a198931bb3a7ee2d491
Rob Mellor <rob@MacBookAir.broadband>
2025-07-29 20:28:59 +0100

    remove companion logic

--- asset/vpaid_player.js
+++ asset/vpaid_player.js
@@ -1,6 +1,6 @@
 // vpaid_player.js
 (function(window) {
-  // VPAID creative object
+  // VPAID creative constructor
   function VPAIDAd() {
     this.events = {};
     this.slot = null;
@@ -12,6 +12,7 @@
   }
 
   /*** VPAID REQUIRED INTERFACE ***/
+
   VPAIDAd.prototype.handshakeVersion = function(version) {
     return '2.0';
   };
@@ -22,41 +23,52 @@
     this.slot = environmentVars.slot || environmentVars.adSlot;
     this.videoSlot = environmentVars.videoSlot;
 
-    // Emit AdLoaded asynchronously so wrapper can subscribe first
+    // Signal loaded on next tick so wrapper can subscribe
     setTimeout(function() {
       this._emitEvent('AdLoaded');
     }.bind(this), 0);
 
-    // Create interactive container
+    // Build interactive overlay
     var container = document.createElement('div');
     container.id = 'vpaid-ad-container';
     Object.assign(container.style, {
-      position: 'absolute', top: '0', left: '0',
-      width: width + 'px', height: height + 'px', zIndex: '9999',
-      backgroundSize: 'cover', backgroundImage: 'url(http://background.jpg)'
+      position: 'absolute',
+      top: '0', left: '0',
+      width: width + 'px',
+      height: height + 'px',
+      zIndex: '9999',
+      backgroundSize: 'cover',
+      backgroundImage: 'url(http://background.jpg)'
     });
 
-    // Input prompt
     var input = document.createElement('textarea');
     input.id = 'vpaid-input';
     input.placeholder = 'Describe your perfect burger';
-    Object.assign(input.style, { width: '80%', margin: '20px auto', display: 'block' });
+    Object.assign(input.style, {
+      width: '80%',
+      margin: '20px auto',
+      display: 'block'
+    });
 
-    // Submit button
     var button = document.createElement('button');
     button.textContent = 'Submit';
-    Object.assign(button.style, { display: 'block', margin: '10px auto' });
+    Object.assign(button.style, {
+      display: 'block',
+      margin: '10px auto'
+    });
 
     button.addEventListener('click', function() {
       if (!input.value) return;
+      // Remove overlay
       if (container.parentNode) container.parentNode.removeChild(container);
 
-      // Load test MP4
+      // Load & play test video
       var testSrc = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4';
       this.videoSlot.src = testSrc;
       this.videoSlot.load();
       this.videoSlot.play();
 
+      // Fire video events
       this._emitEvent('AdVideoStart');
       this.videoSlot.addEventListener('ended', function() {
         this._emitEvent('AdVideoComplete');
@@ -115,31 +127,64 @@
     this._emitEvent('AdSizeChange');
   };
 
-  /*** GETTERS/SETTERS ***/
+  /*** VPAID GETTERS/SETTERS ***/
+
   VPAIDAd.prototype.getAdDuration = function() {
-    return this.videoSlot && this.videoSlot.duration ? this.videoSlot.duration : 0;
+    return this.videoSlot && !isNaN(this.videoSlot.duration)
+      ? this.videoSlot.duration
+      : 0;
   };
+
   VPAIDAd.prototype.getAdRemainingTime = function() {
-    if (!this.videoSlot || typeof this.videoSlot.currentTime !== 'number') return 0;
-    return Math.max(0, (this.videoSlot.duration || 0) - this.videoSlot.currentTime);
+    if (!this.videoSlot || typeof this.videoSlot.currentTime !== 'number') {
+      return 0;
+    }
+    return Math.max(0, this.videoSlot.duration - this.videoSlot.currentTime);
   };
-  VPAIDAd.prototype.getAdLinear = function() { return true; };
-  VPAIDAd.prototype.getAdExpanded = function() { return this.expanded; };
-  VPAIDAd.prototype.getAdSkippableState = function() { return this.skippableState; };
-  VPAIDAd.prototype.getAdWidth = function() { return this.width; };
-  VPAIDAd.prototype.getAdHeight = function() { return this.height; };
-  VPAIDAd.prototype.getAdVolume = function() { return this.videoSlot ? this.videoSlot.volume : 1; };
-  VPAIDAd.prototype.setAdVolume = function(v) { if (this.videoSlot) this.videoSlot.volume = v; };
-  VPAIDAd.prototype.getAdIcons = function() { return []; };
+
+  VPAIDAd.prototype.getAdLinear = function() {
+    return true;
+  };
+
+  VPAIDAd.prototype.getAdExpanded = function() {
+    return this.expanded;
+  };
+
+  VPAIDAd.prototype.getAdSkippableState = function() {
+    return this.skippableState;
+  };
+
+  VPAIDAd.prototype.getAdWidth = function() {
+    return this.width;
+  };
+
+  VPAIDAd.prototype.getAdHeight = function() {
+    return this.height;
+  };
+
+  VPAIDAd.prototype.getAdVolume = function() {
+    return this.videoSlot ? this.videoSlot.volume : 1;
+  };
+
+  VPAIDAd.prototype.setAdVolume = function(v) {
+    if (this.videoSlot) this.videoSlot.volume = v;
+  };
+
+  VPAIDAd.prototype.getAdIcons = function() {
+    return [];
+  };
 
   /*** OVERRIDDEN SUBSCRIBE & UNSUBSCRIBE ***/
+
   VPAIDAd.prototype.subscribe = function(arg1, arg2, arg3) {
     var eventType, callback, context;
+    // wrapper-style: subscribe(callback, eventName, context)
     if (typeof arg1 === 'function' && typeof arg2 === 'string') {
       callback  = arg1;
       eventType = arg2;
       context   = arg3 || this;
     } else {
+      // creative-style: subscribe(eventType, callback)
       eventType = arg1;
       callback  = arg2;
       context   = this;
@@ -159,15 +204,20 @@
     }
     var handlers = this.events[eventType] || [];
     for (var i = handlers.length - 1; i >= 0; i--) {
-      if (handlers[i] === callback) handlers.splice(i, 1);
+      if (handlers[i] === callback) {
+        handlers.splice(i, 1);
+      }
     }
   };
 
   /*** INTERNAL ***/
+
   VPAIDAd.prototype._emitEvent = function(eventType) {
     (this.events[eventType] || []).forEach(function(fn) { fn(); });
   };
 
   // Expose factory
-  window.getVPAIDAd = function() { return new VPAIDAd(); };
+  window.getVPAIDAd = function() {
+    return new VPAIDAd();
+  };
 })(window);
